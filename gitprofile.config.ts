927c4b28dda2be81ac12c48a3de0bea21aa7d01f
Yarne Goossens <90728414+Yarne-Goossens@users.noreply.github.com>
2024-05-28 20:18:22 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -201,11 +201,11 @@ const CONFIG = {
 
     // Custom theme, applied to `procyon` theme
     customTheme: {
-      primary: '#fc055b',
+      primary: '#6c76ea',
       secondary: '#219aaf',
       accent: '#e8d03a',
-      neutral: '#2A2730',
-      'base-100': '#E3E3ED',
+      neutral: '#ffffff',
+      'base-100': '#15191e',
       '--rounded-box': '3rem',
       '--rounded-btn': '3rem',
     },
