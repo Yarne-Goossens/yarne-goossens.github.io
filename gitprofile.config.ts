40befe443e5411c3f98596b7236cd2dabb17cfb6
Yarne Goossens <90728414+Yarne-Goossens@users.noreply.github.com>
2024-05-28 20:25:18 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -206,8 +206,6 @@ const CONFIG = {
       accent: '#e8d03a',
       neutral: '#ffffff',
       'base-100': '#15191e',
-      '--rounded-box': '3rem',
-      '--rounded-btn': '3rem',
     },
   },
 
